fixed infor leakage issue

data_util.py
@@ -1,19 +1,20 @@
 import numpy as np
 from scipy.sparse import csr_matrix
 
-def process_files(files):
+def process_files(files, relation2id=None):
     '''
     files: Dictionary map of file paths to read the triplets from.
     saved_relation2id: Saved relation2id (mostly passed from a trained model) which can be used to map relations to pre-defined indices and filter out the unknown ones.
     '''
     entity2id = {}
-    relation2id = {}
+    if relation2id is None:
+        relation2id = {}
 
     converted_triplets = {}
-    rel_list = []
+    rel_list = [[] for i in range(len(relation2id))]
 
     ent = 0
-    rel = 0
+    rel = len(relation2id)
 
     for file_type, file_path in files.items():
 
@@ -34,7 +35,6 @@ def process_files(files):
                 rel_list.append([])
 
             data.append([entity2id[triplet[0]], relation2id[triplet[1]], entity2id[triplet[2]]])
-            rel_list[relation2id[triplet[1]]].append([entity2id[triplet[0]], entity2id[triplet[2]]])
         
         if file_type == "train":
             for trip in data:
@@ -48,6 +48,9 @@ def process_files(files):
     adj_list = []
     for rel_mat in rel_list:
         rel_array = np.array(rel_mat)
-        adj_list.append(csr_matrix((np.ones(len(rel_mat)),(rel_array[:,0],rel_array[:,1])), shape=(len(entity2id),len(entity2id))))
+        if len(rel_array)==0:
+            adj_list.append(csr_matrix((len(entity2id),len(entity2id))))
+        else:
+            adj_list.append(csr_matrix((np.ones(len(rel_mat)),(rel_array[:,0],rel_array[:,1])), shape=(len(entity2id),len(entity2id))))
 
     return adj_list, converted_triplets, entity2id, relation2id, id2entity, id2relation

datasets.py
@@ -43,7 +43,7 @@ class SubgraphDatasetTrain(Dataset):
         nodes = [link[0] for link in neg_links] + [link[2] for link in neg_links] + [pos_link[0], pos_link[2]]
         node_set = set(nodes)
         logging.debug(f'prepare nodes:{time.time()-st}')
-        sample_nodes = extract_neighbor_nodes(node_set, self.adj_mat, h=self.params.hop, max_nodes_per_hop=10000)
+        sample_nodes = extract_neighbor_nodes(node_set, self.adj_mat, h=self.params.hop, max_nodes_per_hop=500)
         sample_nodes = list(node_set) + list(sample_nodes)
         logging.debug(f'sample nodes:{time.time()-st}')
         main_subgraph = self.graph.subgraph(sample_nodes)
@@ -105,15 +105,17 @@ class SubgraphDatasetTrain(Dataset):
 
 
 class SubgraphDatasetVal(Dataset):
-    def __init__(self, triplets, params, adj_list, num_rels, num_entities, neg_link_per_sample=1):
-        self.edges = triplets
+    def __init__(self, triplets, dataset, params, adj_list, num_rels, num_entities, graph=None, neg_link_per_sample=1):
+        self.edges = triplets[dataset]
         self.adj_list = adj_list
         self.coo_adj_list = [adj.tocoo() for adj in self.adj_list]
         self.num_edges = len(self.edges)
         self.num_nodes = num_entities
         self.num_rels = num_rels
         self.params = params
-        self.graph = construct_graph_from_edges(self.edges.T, self.num_nodes)
+        self.graph = graph
+        if self.graph is None:
+            self.graph = construct_graph_from_edges(triplets[dataset].T, self.num_nodes)
         self.adj_mat = self.graph.adjacency_matrix(transpose=False, scipy_fmt='csr')
         self.adj_mat += self.adj_mat.T
 
@@ -137,7 +139,7 @@ class SubgraphDatasetVal(Dataset):
         neg_links = neg_tail_links+neg_head_links
         nodes = [link[0] for link in neg_links] + [link[2] for link in neg_links] + [pos_link[0], pos_link[2]]
         node_set = set(nodes)
-        sample_nodes = extract_neighbor_nodes(node_set, self.adj_mat, h=self.params.hop, max_nodes_per_hop=10000)
+        sample_nodes = extract_neighbor_nodes(node_set, self.adj_mat, h=self.params.hop, max_nodes_per_hop=2500)
         sample_nodes = list(node_set) + list(sample_nodes)
         main_subgraph = self.graph.subgraph(sample_nodes)
         main_subgraph.edata['type'] = self.graph.edata['type'][main_subgraph.edata[dgl.EID]]
@@ -147,8 +149,10 @@ class SubgraphDatasetVal(Dataset):
         node_to_id = {pid: i for i, pid in enumerate(p_id)}
         can_edges = [pos_link]+neg_links
         graphs = []
-        for edge in can_edges:
+        for i, edge in enumerate(can_edges):
             pos_nodes, pos_label, _, _, _ = subgraph_extraction_labeling_wiki([node_to_id[edge[0]], node_to_id[edge[2]]], rel, local_adj_mat, h=self.params.hop, max_nodes_per_hop=self.params.max_nodes_per_hop)
+            # if i == 0 and len(pos_nodes)==2:
+            #     print(index)
             pos_subgraph = main_subgraph.subgraph(pos_nodes)
             pos_subgraph.edata['type'] = main_subgraph.edata['type'][pos_subgraph.edata[dgl.EID]]
             pos_subgraph.edata['label'] = torch.tensor(rel * np.ones(pos_subgraph.edata['type'].shape),

managers/evaluator.py
@@ -19,6 +19,7 @@ class Evaluator():
     def eval(self, save=False):
         # print("eval")
         mrr_scores = []
+        h10_scores = []
         if save:
             dataloader = DataLoader(self.data, batch_size=self.params.val_batch_size, num_workers=self.params.num_workers, collate_fn=self.params.collate_fn_val, prefetch_factor=self.params.prefetch_val, pin_memory=True)
             num_batches = len(dataloader)
@@ -42,14 +43,19 @@ class Evaluator():
                     score_pos = self.graph_classifier(data_pos)
                     scores = score_pos.view(len(targets_pos), -1)
                     scores = scores.cpu().numpy()
+                    (b_size, dim_size) = scores.shape
                     if save:
                         all_scores[self.params.val_batch_size*batch_count:self.params.val_batch_size*(batch_count+1),:]= scores
                     else:
                         tp = targets_pos.cpu().numpy()
 
                         true_labels = np.zeros(scores.shape)
-                        true_labels[np.arange(len(targets_pos)), tp] = 1
+                        true_labels[np.arange(b_size), tp] = 1
 
+                        ranking = np.argsort(scores, axis=1)
+                        h10_ranking = ranking[:,10:]
+                        true_ranking = np.sum(h10_ranking==tp[:,np.newaxis], axis=1)
+                        h10_scores.append(np.mean(true_ranking))
                         mrr_scores.append(metrics.label_ranking_average_precision_score(true_labels, scores))
                 except RuntimeError:
                     print("oom")
@@ -79,4 +85,9 @@ class Evaluator():
                 mrr += v
             if len(mrr_scores)!=0:
                 mrr /= len(mrr_scores)
-            return {'mrr': mrr}
+            h10 = 0
+            for v in h10_scores:
+                h10+=v
+            if len(h10_scores)!=0:
+                h10 /= len(h10_scores)
+            return {'mrr': mrr, 'h10': h10}

train_benchmark_inductive.py
@@ -16,11 +16,12 @@ class Mem:
     def __init__(self):
         self.hop = 3
         self.enclosing_sub_graph = False
-        self.max_nodes_per_hop = 40
+        self.max_nodes_per_hop = 30
         self.num_neg_samples_per_link = 2
         self.root_path = "/project/tantra/jerry.kong/ogb_project/ogb-grail-mod/data"
         self.data_path = "/project/tantra/jerry.kong/ogb_project/ogb-grail-mod/data"
-        self.data_set = "WN18RR"
+        self.data_set = "WN18RR_v1"
+        self.ind_data_set = "WN18RR_v1_ind"
         self.num_rels = 1315
         self.rel_emb_dim = 48
         self.add_ht_emb = True
@@ -40,12 +41,12 @@ class Mem:
         self.lr = 0.0001
         self.l2 = 1e-3
         self.batch_size = 16
-        self.num_workers = 16
+        self.num_workers = 8
         self.num_epochs = 20
         self.save_every = 1
         self.margin = 5
         self.train_edges = 10000
-        self.val_size = 500
+        self.val_size = 1000
         self.eval_every_iter = 1
         self.early_stop = 5
         self.split = 'val'
@@ -86,9 +87,19 @@ if __name__ == '__main__':
         files[f] = osp.join(database_path,f'{f}.txt')
 
     adj_list, converted_triplets, entity2id, relation2id, id2entity, id2relation = process_files(files)
-    params.num_entities = len(entity2id)
-    params.num_rels = len(relation2id)
-    print(f'Dataset {params.data_set} has {params.num_entities} entities and {params.num_rels} relations')
+    train_num_entities = len(entity2id)
+    train_num_rel = len(relation2id)
+    print(f'Dataset {params.data_set} has {train_num_entities} entities and {train_num_rel} relations')
+
+    ind_database_path = osp.join(params.data_path, params.ind_data_set)
+    files = {}
+    for f in params.use_data:
+        files[f] = osp.join(ind_database_path,f'{f}.txt')
+
+    adj_list_ind, converted_triplets_ind, entity2id_ind, relation2id_ind, id2entity_ind, id2relation_ind = process_files(files, relation2id=relation2id)
+    ind_num_entities = len(entity2id_ind)
+    ind_num_rel = len(relation2id_ind)
+    print(f'Dataset {params.ind_data_set} has {ind_num_entities} entities and {ind_num_rel} relations')
 
     params.collate_fn = collate_dgl
     params.collate_fn_val = collate_dgl_val
@@ -96,8 +107,8 @@ if __name__ == '__main__':
     params.move_batch_to_device_val = move_batch_to_device_dgl_val
     torch.multiprocessing.set_sharing_strategy('file_system')
 
-    train = SubgraphDatasetTrain(converted_triplets['train'], params, adj_list, params.num_rels, params.num_entities, neg_link_per_sample=25)
-    val = SubgraphDatasetVal(converted_triplets, 'valid', params, adj_list, params.num_rels, params.num_entities, graph=train.graph, neg_link_per_sample=25)
+    train = SubgraphDatasetTrain(converted_triplets['train'], params, adj_list, train_num_rel, train_num_entities, neg_link_per_sample=10)
+    val = SubgraphDatasetVal(converted_triplets_ind, 'valid', params, adj_list_ind, ind_num_rel, ind_num_entities, neg_link_per_sample=25)
     params.inp_dim = train.n_feat_dim
     graph_classifier = dgl_model(params, relation2id).to(device=params.device)
 